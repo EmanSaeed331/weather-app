cfe13b0efbb5529134c2a4e9a23b87ec95fcb816
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-21 10:17:29 +0200

    fix:argument

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
 const forCast = require('./utils/forcast')
-const address = process.argv[2]
+const address = process.argv[4]
 if (!address){
     console.log('Please provide address')
 }
 else{
 
     
-geocode(address,(error, data)=>{
+geocode(address,(error, {latitude , longitude , location})=>{
     if(error){
       return console.log(error)
           }
       
-          forCast(data.latitude,data.longitude , (error, forecastData)=>
+          forCast(latitude,longitude , (error, forecastData)=>
           {   
           if(error){
               return console.log(error)
           }
-          console.log(data.location)
+          console.log(location)
           console.log(forecastData.long.current)
       
       
@@ -29,6 +29,7 @@ geocode(address,(error, data)=>{
   
   })
 }
+
 
 
 
--- utils/forcast.js
+++ utils/forcast.js
@@ -1,8 +1,9 @@
 const request = require('request')
-const forCast = (lat , lon , callback)=>{
-    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon='+lon+'&appid=641cff8180ebf3531829ad2be14e0938'
+const forCast = (latitude , longitude , callback)=>{
+    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon='+ longitude +'&appid=641cff8180ebf3531829ad2be14e0938'
+    const uu = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon='+ longitude +'&exclude=daily&appid=641cff8180ebf3531829ad2be14e0938'
 
-    request({url:url,json:true},(error, response)=>{
+    request({url:uu,json:true},(error, response)=>{
         if (error){
             callback('Unable to connect to location services!', undefined)
 
