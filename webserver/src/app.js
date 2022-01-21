88ae392677c065a69023d4591e249b64bcacda6d
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-21 12:26:41 +0200

    fix:directory

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -5,8 +5,7 @@ console.log(path.join(__dirname,'../public'))
 const app =express()
 //To make express use the static page 
 const publicDirPath = path.join(__dirname,'../public')
-const helpDirPath = path.join(__dirname,'../public')
-const aboutDirPath = path.join(__dirname,'../public')
+
 
 app.use(express.static(publicDirPath))
 
@@ -17,13 +16,11 @@ app.get('/',(request,response)=>{
 })
 app.get('/help',(request,response)=>{
     response.send('Help Page !')
-    app.use(express.static(helpDirPath))
 
 
 })
 app.get('/about',(request,response)=>{
     response.send('<h1>About Page !</h1>')
-    app.use(express.static(aboutDirPath))
 
 
 })
